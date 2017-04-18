df28db9615c9ca5ed095de21a65c850cf69ae466
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-04-18 18:40:55 +0300

    Player Notification

--- chesslib/Player/Interfaces/IPlayer.cs
+++ chesslib/Player/Interfaces/IPlayer.cs
@@ -7,12 +7,14 @@ using System.Text;
 
 namespace chesslib.Player
 {
-    public interface IPlayer
+    public interface IPlayer : IObserver<bool>
     {
         PlayerType PlayerType { get; set; }
         MakeMoveCommand MakeMoveCommand { get; set; }
 
         IStrategy Strategy { get; set; }
+        Game Game { get; set; }
+
         void MakeMove();
     }
 }
--- chesslib/Player/RealPlayer.cs
+++ chesslib/Player/RealPlayer.cs
@@ -8,24 +8,34 @@ using chesslib.Strategy;
 
 namespace chesslib.Player
 {
-    public class RealPlayer : IPlayer
+    public class RealPlayer : IPlayer, IObservable<RealPlayer>
     {
         private Game _game;
+        private IStrategy strategy;
+        public Game Game
+        {
+            get { return _game; }
+            set { _game = value; }
+        }
 
-        public RealPlayer(PlayerType playerType, Game game)
+        public RealPlayer(PlayerType playerType)
         {
             PlayerType = playerType;
-            _game = game;
+            _observers = new List<IObserver<RealPlayer>>();
         }
 
         public PlayerType PlayerType { get; set; }
         public MakeMoveCommand MakeMoveCommand { get; set; }
-        public IStrategy Strategy { get; set; }
+        public IStrategy Strategy
+        {
+            get { return strategy; }
+            set { strategy = value; }
+        }
 
         public void PrepareMove()
         {
             var move = Strategy.PrepareMove();
-            MakeMoveCommand = new MakeMoveCommand(this, move.Item1, move.Item2, _game);
+            MakeMoveCommand = new MakeMoveCommand(this, move.Item1, move.Item2, Game);
         }
 
         public void MakeMove()
@@ -42,5 +52,40 @@ namespace chesslib.Player
         {
             MakeMoveCommand = null;
         }
+        #region Game Observer
+
+        public void OnNext(bool value)
+        {
+            if (value)
+                NotifyForMoveInput();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region IObservable
+        private List<IObserver<RealPlayer>> _observers;
+        public IDisposable Subscribe(IObserver<RealPlayer> observer)
+        {
+            if (!_observers.Contains(observer))
+                _observers.Add(observer);
+            return new Unsubscriber<RealPlayer>(_observers, observer);
+        }
+        private void NotifyForMoveInput()
+        {
+            foreach (var obs in _observers)
+            {
+                obs.OnNext(this);
+            }
+        }
+        #endregion
     }
 }
