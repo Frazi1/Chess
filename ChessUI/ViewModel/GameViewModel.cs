0a3d17c49b2de33469ac4e7a51ffb50579c6eb9a
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-04-29 13:13:05 +0300

    No cahnges

--- ChessUI/ViewModel/GameViewModel.cs
+++ ChessUI/ViewModel/GameViewModel.cs
@@ -28,17 +28,17 @@ namespace ChessUI.ViewModel
             Game = new Game();
 
             //TODO: передалать
-            //RealPlayer p1 = new RealPlayer(PlayerType.White);
+            RealPlayer p1 = new RealPlayer(PlayerType.White);
             RealPlayer p2 = new RealPlayer(PlayerType.Black);
-            ComputerPlayer p1 = new ComputerPlayer(PlayerType.White);
+            //ComputerPlayer p1 = new ComputerPlayer(PlayerType.White);
             //ComputerPlayer p2 = new ComputerPlayer(PlayerType.Black);
             Game.AddPlayer(p1);
             Game.AddPlayer(p2);
-            p1.Strategy = new DefaultComputerStrategy(p1);
+            //p1.Strategy = new DefaultComputerStrategy(p1);
             //p2.Strategy = new DefaultComputerStrategy(p2);
 
 
-            //RealPlayersViewModels.Add(new RealPlayerViewModel(p1, this));
+            RealPlayersViewModels.Add(new RealPlayerViewModel(p1, this));
             RealPlayersViewModels.Add(new RealPlayerViewModel(p2, this));
 
             //
