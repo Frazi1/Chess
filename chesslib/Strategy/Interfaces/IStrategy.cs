8e163c1130ca594ba14c746db7d0ffe25b157c62
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-05-02 23:18:43 +0300

    IPlayer rework

--- chesslib/Strategy/Interfaces/IStrategy.cs
+++ chesslib/Strategy/Interfaces/IStrategy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using chesslib.Field;
+using chesslib.Player;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +9,6 @@ namespace chesslib.Strategy
 {
     public interface IStrategy
     {
-        Tuple<Piece,Cell> PrepareMove();
+        Tuple<Piece,Cell> PrepareMove(IPlayer player, Board board);
     }
 }
