246070002f74b8ff7430d26f1fbac55f79ad4765
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-05-02 23:22:12 +0300

    Rename

---
+++ ChessUI/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,30 @@
+﻿using chesslib;
+using chesslib.Player;
+using chesslib.Strategy;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessUI.ViewModel
+{
+    public class PlayerViewModel : ViewModelBase
+    {
+        private GameViewModel _gameViewModel;
+        public IPlayer Player { get; set; }
+        public PlayerViewModel(IPlayer player, GameViewModel gvm)
+        {
+            _gameViewModel = gvm;
+            Player = player;
+        }
+        public void PushCommand()
+        {
+            //if (!_gameViewModel.IsPaused)
+                Player.MakeMoveCommand = new chesslib.Command.MakeMoveCommand(Player.PlayerColor,
+                    _gameViewModel.SelectedPiece.Piece,
+                    _gameViewModel.NextCell);
+        }
+
+    }
+}
