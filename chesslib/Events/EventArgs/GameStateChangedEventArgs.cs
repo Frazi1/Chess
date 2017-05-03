154391dd5d708516154fc5b9c3a28861b2733b27
Вычиков Дмитрий Дмитриевич <diman896@gmail.com>
2017-05-03 23:32:36 +0300

    Fixes

--- chesslib/Events/EventArgs/GameStateChangedEventArgs.cs
+++ chesslib/Events/EventArgs/GameStateChangedEventArgs.cs
@@ -1,8 +1,9 @@
-﻿using chesslib.Utils;
+﻿using System;
+using chesslib.Utils;
 
 namespace chesslib.Events
 {
-    public class GameStateChangedEventArgs
+    public class GameStateChangedEventArgs : EventArgs
     {
         public Game Game { get; set; }
         public bool IsCheck { get { return BoardUtils.IsCheck(Game.Board, Game.CurrentPlayer.PlayerColor); } }
--- chesslib/Utils/GameFactory/GameCreationParams.cs
+++ chesslib/Utils/GameFactory/GameCreationParams.cs
@@ -1,9 +1,11 @@
-﻿namespace chesslib.Utils
+﻿using chesslib.Player;
+
+namespace chesslib.Utils
 {
     public class GameCreationParams
     {
-        public Player.IPlayer Player1 { get; set; }
-        public Player.IPlayer Player2 { get; set; }
+        public IPlayer Player1 { get; set; }
+        public IPlayer Player2 { get; set; }
         public bool LoadFromFile { get; set; }
         public string Path { get; set; }
 
