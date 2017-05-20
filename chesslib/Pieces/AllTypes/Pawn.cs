10e07ddf34f326cbef63527ee84c76330ee1d16d
Dmitri Vychikov <diman896@gmail.com>
2017-05-20 16:04:02 +0300

    Pawn move pattern rework

--- chesslib/Pieces/AllTypes/Pawn.cs
+++ chesslib/Pieces/AllTypes/Pawn.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using chesslib.Field;
+using chesslib.Utils;
 
 namespace chesslib.Figures
 {
     [Serializable]
     public class Pawn : Piece
     {
+        public int Direction { get; }
         public Pawn(Cell currentCell, PlayerColor playerColor, Board board) : base(currentCell, playerColor, board)
         {
             IsPromoted = false;
             PieceType = PieceType.Pawn;
+            Direction = playerColor == PlayerColor.White ? -1 : 1;
         }
 
         public bool IsPromoted { get; set; }
@@ -20,36 +23,16 @@ namespace chesslib.Figures
             int x = CurrentCell.PosX;
             int y = CurrentCell.PosY;
 
-            int size = Board.Size;
-
-            if (PlayerColor == PlayerColor.White)
+            //Атака вперед влево
+            if (BoardUtils.IsValidCell(x - 1, y + Direction))
             {
-                //Атака вперед влево
-                if (y > 0 && x > 0)
-                {
-                    yield return Board.GetCell(x - 1, y - 1);
-                }
-
-                //Атака вперед вправо
-                if (y > 0 && x < size - 1)
-                {
-                    yield return Board.GetCell(x + 1, y - 1);
-                }
+                yield return Board.GetCell(x - 1, y + Direction);
             }
-            else if (PlayerColor == PlayerColor.Black)
+
+            //Атака вперед вправо
+            if (BoardUtils.IsValidCell(x + 1, y + Direction))
             {
-
-                //Атака вперед влево
-                if (y <size-1 && x > 0)
-                {
-                    yield return Board.GetCell(x - 1, y + 1);
-                }
-
-                //Атака вперед вправо
-                if (y > 0 && x < size - 1)
-                {
-                    yield return Board.GetCell(x + 1, y + 1);
-                }
+                yield return Board.GetCell(x + 1, y + Direction);
             }
         }
 
@@ -57,83 +40,41 @@ namespace chesslib.Figures
         {
             int x = PosX;
             int y = PosY;
-            int size = Board.Size;
 
+            if (!BoardUtils.IsValidCell(x, y + Direction))
+                yield break;
 
-
-            if (PlayerColor == PlayerColor.White)
+            Cell cell1 = Board.GetCell(x, y + Direction);
+            if (!cell1.IsTaken)
             {
-                if (y > 0)
-                {
-                    Cell cell1 = Board.GetCell(x, y - 1);
-                    if (!cell1.IsTaken)
-                    {
-                        yield return cell1;
-                        Cell cell2 = Board.GetCell(x, y - 2);
-                        if (!HasAlreadyMoved && !cell2.IsTaken)
-                            yield return cell2;
-                    }
-
-                    //Атака вперед влево
-                    if (y > 0 && x > 0)
-                    {
-                        Cell cell = Board.GetCell(x - 1, y - 1);
-                        if (cell.IsTaken &&
-                            cell.Piece.PlayerColor != PlayerColor)
-                        {
-                            yield return cell;
-                        }
-                    }
-
-                    //Атака вперед вправо
-                    if (y > 0 && x < size - 1)
-                    {
-                        Cell cell = Board.GetCell(x + 1, y - 1);
-                        if (cell.IsTaken &&
-                            cell.Piece.PlayerColor != PlayerColor)
-                        {
-                            yield return cell;
-                        }
-                    }
-                }
+                yield return cell1;
+                Cell cell2 = Board.GetCell(x, y + 2 * Direction);
+                if (!HasAlreadyMoved && !cell2.IsTaken)
+                    yield return cell2;
             }
-            else if (PlayerColor == PlayerColor.Black)
+
+            //Атака вперед влево
+            if (BoardUtils.IsValidCell(x-1,y+Direction))
             {
-                if (y < size - 1)
+                Cell cell = Board.GetCell(x - 1, y + Direction);
+                if (cell.IsTaken &&
+                    cell.Piece.PlayerColor != PlayerColor)
                 {
-                    Cell cell1 = Board.GetCell(x, y + 1);
-                    if (!cell1.IsTaken)
-                    {
-                        yield return cell1;
-                        Cell cell2 = Board.GetCell(x, y + 2);
-                        if (!HasAlreadyMoved && !cell2.IsTaken)
-                            yield return cell2;
-                    }
-
-                    //Атака вперед влево
-                    if (y > 0 && x > 0)
-                    {
-                        Cell cell = Board.GetCell(x - 1, y + 1);
-                        if (cell.IsTaken &&
-                            cell.Piece.PlayerColor != PlayerColor)
-                        {
-                            yield return cell;
-                        }
-                    }
-
-                    //Атака вперед вправо
-                    if (y > 0 && x < size - 1)
-                    {
-                        Cell cell = Board.GetCell(x + 1, y + 1);
-                        if (cell.IsTaken &&
-                            cell.Piece.PlayerColor != PlayerColor)
-                        {
-                            yield return cell;
-                        }
-                    }
+                    yield return cell;
                 }
             }
 
+            //Атака вперед вправо
+            if (BoardUtils.IsValidCell(x + 1, y + Direction))
+            {
+                Cell cell = Board.GetCell(x + 1, y + Direction);
+                if (cell.IsTaken &&
+                    cell.Piece.PlayerColor != PlayerColor)
+                {
+                    yield return cell;
+                }
+            }
         }
     }
 }
+
